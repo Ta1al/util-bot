53ab4d86c69b6c05df4f465dac202df717f6e9c9
Ta1al <talalahmad257@gmail.com>
2022-08-02 15:02:31 +0000

    Add discord-api-types

--- src/handler.ts
+++ src/handler.ts
@@ -1,8 +1,15 @@
 // Command Handler
-import { InteractionType } from "discord-interactions";
-
-async function handle(message: InteractionType.APPLICATION_COMMAND) {
-  // Handle the message
+import { InteractionResponseType } from "discord-interactions";
+import { APIChatInputApplicationCommandInteraction } from "discord-api-types/v10";
+async function handle(message: any) {
+  const interaction = message as APIChatInputApplicationCommandInteraction;
+  const commandName = interaction.data.name;
+  return {
+    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+    data: {
+      content: commandName,
+    },
+  };
 }
 
 export default handle;

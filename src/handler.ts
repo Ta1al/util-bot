42cb42282d132fedf0c9e78f043ca19e6164d126
Ta1al <talalahmad257@gmail.com>
2022-08-03 15:54:44 +0000

    Handle commands

--- src/handler.ts
+++ src/handler.ts
@@ -1,13 +1,22 @@
 // Command Handler
 import { APIChatInputApplicationCommandInteraction, APIInteractionResponse, InteractionResponseType } from "discord-api-types/v10";
+
 async function handle(interaction: APIChatInputApplicationCommandInteraction): Promise<APIInteractionResponse> {
+  let response;
   const commandName = interaction.data.name;
-  return {
-    type: InteractionResponseType.ChannelMessageWithSource,
-    data: {
-      content: commandName,
-    },
-  };
+  try {
+    response = await require(`./commands/${commandName}`).exec(interaction);
+  } catch (error) {
+    console.error(error);
+    response = {
+      type: InteractionResponseType.ChannelMessageWithSource,
+      data: {
+        content: '⚠️ An error occurred while executing this command!',
+        ephemeral: true,
+      },
+    }
+  }
+  return response;
 }
 
 export default handle;

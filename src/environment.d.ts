7fd493ed02f1c5b9db8b35d8db6a02746a501c57
Ta1al <talalahmad257@gmail.com>
2022-08-04 03:17:59 +0000

    types

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -1,2 +1,6 @@
-declare module 'to-zalgo'
-declare module 'to-zalgo/banish'
+declare module 'to-zalgo' {
+  export function toZalgo(text: string): string;
+}
+declare module 'to-zalgo/banish' {
+  export function banish(text: string): string;
+}

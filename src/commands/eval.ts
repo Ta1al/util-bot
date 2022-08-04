50120875b040c267d044be26ec781f74c34b9ca5
Ta1al <talalahmad257@gmail.com>
2022-08-04 03:59:04 +0000

    defer response

--- src/commands/eval.ts
+++ src/commands/eval.ts
@@ -1,10 +1,13 @@
 import {
   APIApplicationCommandInteractionDataStringOption as StringOption,
+  APIApplicationCommandInteractionDataIntegerOption as IntegerOption,
+  APIApplicationCommandInteractionDataBooleanOption as BooleanOption,
   APIChatInputApplicationCommandInteraction as Interaction,
   APIInteractionResponse as Response,
   ApplicationCommandOptionType as OptionType,
   InteractionResponseType as ResponseType,
-  RESTPostAPIChatInputApplicationCommandsJSONBody as Command
+  RESTPostAPIChatInputApplicationCommandsJSONBody as Command,
+  MessageFlags
 } from "discord-api-types/v10";
 
 const commandData: Command = {
@@ -32,4 +35,25 @@ const commandData: Command = {
   dm_permission: false
 };
 
-export { commandData };
+const exec = async (interaction: Interaction): Promise<Response> => {
+  if(interaction.member?.user.id !== process.env.OWNER_ID) return {
+    type: ResponseType.ChannelMessageWithSource,
+    data: {
+      content: "You are not allowed to use this command",
+      flags: MessageFlags.Ephemeral
+    }
+  }
+
+  const code = <StringOption>interaction.data.options!.find(({ name }) => name === "code")!;
+  const depth = <IntegerOption>interaction.data.options!.find(({ name }) => name === "depth")!;
+  const ephemeral = <BooleanOption>interaction.data.options!.find(({ name }) => name === "ephemeral")!;
+
+  return {
+    type: ResponseType.DeferredChannelMessageWithSource,
+    data: {
+      flags: ephemeral.value ? MessageFlags.Ephemeral : undefined
+    }
+  };
+}
+
+export { commandData, exec };

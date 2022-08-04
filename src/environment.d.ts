eb42936baf8dd98957d22c4698d5d45740952e70
Ta1al <talalahmad257@gmail.com>
2022-08-04 03:19:09 +0000

    Fix export

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -1,6 +1,6 @@
 declare module 'to-zalgo' {
-  export function toZalgo(text: string): string;
+  export default function zalgo(text: string): string;
 }
 declare module 'to-zalgo/banish' {
-  export function banish(text: string): string;
+  export default function unzalgo(text: string): string;
 }

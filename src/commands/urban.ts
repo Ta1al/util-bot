f1eff0642b0e067ae35317172450685b74dd2ba3
Talal <talalahmad257@gmail.com>
2022-08-16 09:52:58 +0000

    Add select menu functionality

--- src/commands/urban.ts
+++ src/commands/urban.ts
@@ -1,13 +1,14 @@
 import {
   APIChatInputApplicationCommandInteraction as Interaction,
   RESTPostAPIChatInputApplicationCommandsJSONBody as Command,
-  APIInteractionResponseDeferredChannelMessageWithSource as Response,
   RESTPatchAPIInteractionOriginalResponseJSONBody as Patch,
   InteractionResponseType as ResponseType,
-  APIApplicationCommandInteractionDataStringOption as StringOption
+  APIApplicationCommandInteractionDataStringOption as StringOption,
+  APIMessageComponentSelectMenuInteraction as SelectMenuInteraction
 } from "discord-api-types/v10";
 import fetch from "node-fetch";
 import ud from "urban-dictionary";
+import emitter from "../index";
 
 const commandData: Command = {
   name: "urban",
@@ -23,49 +24,87 @@ const commandData: Command = {
 };
 
 const exec = async (interaction: Interaction, res: any) => {
-  const response: Response = {
-    type: ResponseType.DeferredChannelMessageWithSource
-  };
-  await res.send(response);
+  await res.send({ type: ResponseType.DeferredChannelMessageWithSource });
 
   const { value: term } = <StringOption>interaction.data.options!.find(o => o.name === "term")!;
 
-  ud.define(term)
-    .then(defs => {
-      const def = defs[0];
-      const obj: Patch = {
-        embeds: [
-          {
-            author: { name: `Author: ${def.author}` },
-            title: `${def.word}`,
-            description: def.definition.replace(/\[([^\]]+)\]/g, "$1").slice(0, 4096),
-            url: def.permalink,
-            color: 0x2f3136,
-            footer: { text: `${def.thumbs_up} 👍 | ${def.thumbs_down} 👎` },
-            timestamp: def.written_on,
-            fields: [
-              {
-                name: "Example",
-                value: def.example.replace(/\[([^\]]+)\]/g, "$1").slice(0, 1024) || "N/A"
-              }
-            ]
-          }
-        ]
-      };
-      update(interaction, obj)
-    })
-    .catch(e => {
-      console.error(e);
-      const obj: Patch = {
-        content: `⚠ An error occurred.`
-      };
-      update(interaction, obj);
+  try {
+    const definitions = await ud.define(term);
+    update(interaction, {
+      components: [makeSelectMenu(definitions)],
+      embeds: [makeEmbed(definitions[0])]
     });
+
+    const callback = (select: SelectMenuInteraction, selectRes: any) =>
+      handleSelection(select, selectRes, interaction, definitions);
+
+    emitter.on(interaction.id, callback);
+    setTimeout(() => emitter.removeListener(interaction.id, callback), 60000);
+  } catch (error) {
+    update(interaction, { content: `⚠ Definition not found!` });
+  }
 };
 
-function update(interaction: Interaction, obj: Patch) {
+async function handleSelection(
+  select: SelectMenuInteraction,
+  res: any,
+  interaction: Interaction,
+  definitions: ud.DefinitionObject[]
+) {
+  if (select.member?.user.id !== interaction.member?.user.id) return;
+  if (select.user?.id !== interaction.user?.id) return;
+
+  const value = parseInt(select.data.values[0]);
+  return await res.send({
+    type: ResponseType.UpdateMessage,
+    data: {
+      components: [makeSelectMenu(definitions, value)],
+      embeds: [makeEmbed(definitions[value])]
+    }
+  });
+}
+
+function makeSelectMenu(defs: ud.DefinitionObject[], value = 0) {
+  return {
+    type: 1,
+    components: [
+      {
+        type: 3,
+        custom_id: `urban`,
+        options: Array.from(defs).map((d, i) => {
+          return {
+            label: `${i + 1}`,
+            value: `${i}`,
+            description: `Author: ${d.author}`,
+            default: i === value
+          };
+        })
+      }
+    ]
+  };
+}
+
+function makeEmbed(def: ud.DefinitionObject) {
+  return {
+    author: { name: `Author: ${def.author}` },
+    title: `${def.word}`,
+    description: def.definition.replace(/\[([^\]]+)\]/g, "$1").slice(0, 4096),
+    url: def.permalink,
+    color: 0x2f3136,
+    footer: { text: `${def.thumbs_up} 👍 | ${def.thumbs_down} 👎` },
+    timestamp: def.written_on,
+    fields: [
+      {
+        name: "Example",
+        value: def.example.replace(/\[([^\]]+)\]/g, "$1").slice(0, 1024) || "N/A"
+      }
+    ]
+  };
+}
+
+function update(interaction: Interaction, patch: Patch) {
   const url = `https://discord.com/api/v10/webhooks/${interaction.application_id}/${interaction.token}/messages/@original`;
-  fetch(url, { method: "PATCH", body: JSON.stringify(obj), headers: { "Content-Type": "application/json" } });
+  fetch(url, { method: "PATCH", body: JSON.stringify(patch), headers: { "Content-Type": "application/json" } });
 }
 
 export { commandData, exec };

e2f20cae4026f0bfc777ba2905ecad92e8b80609
Talal <talalahmad257@gmail.com>
2022-08-23 06:31:35 +0000

    Add basic response

--- src/commands/userinfo.ts
+++ src/commands/userinfo.ts
@@ -1,11 +1,28 @@
 import {
   RESTPostAPIContextMenuApplicationCommandsJSONBody as Command,
-  ApplicationCommandType as CommandType
+  ApplicationCommandType as CommandType,
+  APIUserApplicationCommandInteraction as Interaction,
+  InteractionResponseType as ResponseType,
+  MessageFlags
 } from "discord-api-types/v10";
+import { respond } from "../util";
 
 const commandData: Command = {
   type: CommandType.User,
   name: "userinfo"
 };
 
-export { commandData };
+const exec = async (interaction: Interaction, res: any) => {
+  const data = interaction.data;
+  const user = data.resolved.users[data.target_id]!;
+
+  respond(res, {
+    type: ResponseType.ChannelMessageWithSource,
+    data: {
+      content: `**User:** ${user.username}#${user.discriminator} (${user.id})`,
+      flags: MessageFlags.Ephemeral
+    }
+  });
+};
+
+export { commandData, exec };

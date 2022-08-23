0e03af950ffaeae49906bd210b0dbe65208a698e
Talal <talalahmad257@gmail.com>
2022-08-23 06:02:09 +0000

    Add updateMessageWithAttachment

--- src/util/index.ts
+++ src/util/index.ts
@@ -6,7 +6,8 @@ import {
 } from "discord-api-types/v10";
 import fetch from "node-fetch";
 import escapeMarkdown from "./markdownEscape";
-import emitter from './emitter';
+import emitter from "./emitter";
+import FormData from "form-data";
 
 async function respond(res: any, body: Response) {
   return await res.send(body);
@@ -28,4 +29,26 @@ async function updateMessage(
     .catch(e => e);
 }
 
-export { respond, updateMessage, escapeMarkdown, emitter };
+async function updateMessageWithAttachment(
+  body: Patch,
+  applicationId: string,
+  token: string,
+  attachment: { name: string; file: Buffer }[],
+  messageId: string | undefined = undefined
+): Promise<any> {
+  const url = `${RouteBases.api}${Routes.webhookMessage(applicationId, token, messageId)}`;
+
+  const form = new FormData();
+  body.attachments = [];
+  attachment.forEach((a, i) => body.attachments?.push({ id: `${i}`, filename: a.name }));
+  form.append("payload_json", JSON.stringify(body));
+  attachment.forEach((a, i) => form.append(`files[${i}]`, a.file, { filename: a.name }));
+  return fetch(url, {
+    method: "PATCH",
+    body: form
+  })
+    .then(res => res.json())
+    .catch(e => e);
+}
+
+export { respond, updateMessage, escapeMarkdown, emitter, updateMessageWithAttachment };

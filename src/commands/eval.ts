e92ff96fa3795170501459774464081a02bd1f2d
Talal <talalahmad257@gmail.com>
2022-08-23 06:02:16 +0000

    Use util function

--- src/commands/eval.ts
+++ src/commands/eval.ts
@@ -6,15 +6,10 @@ import {
   ApplicationCommandOptionType as OptionType,
   InteractionResponseType as ResponseType,
   RESTPostAPIChatInputApplicationCommandsJSONBody as Command,
-  MessageFlags,
-  RESTPatchAPIInteractionOriginalResponseFormDataBody as Patch,
-  RouteBases,
-  Routes
+  MessageFlags
 } from "discord-api-types/v10";
-import fetch from "node-fetch";
 import util from "util";
-import FormData from "form-data";
-import { respond } from "../util";
+import { respond, updateMessageWithAttachment } from "../util";
 
 const commandData: Command = {
   name: "eval",
@@ -52,8 +47,12 @@ const exec = async (interaction: Interaction, res: any): Promise<void> => {
     });
 
   const code = <StringOption>interaction.data.options!.find(({ name }) => name === "code")!;
-  const depth = <IntegerOption | undefined>interaction.data.options!.find(({ name }) => name === "depth");
-  const ephemeral = <BooleanOption | undefined>interaction.data.options!.find(({ name }) => name === "ephemeral");
+  const depth = <IntegerOption | undefined>(
+    interaction.data.options!.find(({ name }) => name === "depth")
+  );
+  const ephemeral = <BooleanOption | undefined>(
+    interaction.data.options!.find(({ name }) => name === "ephemeral")
+  );
 
   await respond(res, {
     type: ResponseType.DeferredChannelMessageWithSource,
@@ -63,25 +62,13 @@ const exec = async (interaction: Interaction, res: any): Promise<void> => {
   });
   const evaled = await eval(code.value);
   const result = util.inspect(evaled, { depth: depth?.value ?? 0 });
-  update(interaction, result);
+  updateMessageWithAttachment(
+    { content: `\`\`\`js\n${result}\`\`\`` },
+    interaction.application_id,
+    interaction.token,
+    [{ name: "output.txt", file: Buffer.from(result) }]
+  );
 };
 
-async function update(interaction: Interaction, result: string): Promise<void> {
-  let patch: Patch;
-  const url = `${RouteBases.api}${Routes.webhookMessage(interaction.application_id, interaction.token)}`;
-
-  const long = result.length > 1990; // 2000 - 10 (for code block)
-  patch = {
-    content: long ? undefined : `\`\`\`js\n${result}\`\`\``,
-    attachments: long ? [{ id: "0", filename: "output.txt" }] : undefined
-  };
-  const formData = new FormData();
-  formData.append("payload_json", JSON.stringify(patch), { contentType: "application/json" });
-  long ? formData.append("files[0]", Buffer.from(result), { filename: "output.txt" }) : "";
-
-  fetch(url, { method: "PATCH", body: formData });
-  // .then((res) => res.json());
-  // .then(console.log);
-}
 
 export { commandData, exec };

71525674c8eae939cacd059102402c6c5d20453e
Talal <talalahmad257@gmail.com>
2022-11-13 05:15:06 +0000

    Refer to handler

--- src/utils/router.ts
+++ src/utils/router.ts
@@ -1,27 +1,40 @@
 import {
-  APIApplicationCommandInteraction,
-  APIPingInteraction,
+  APIInteraction,
   InteractionResponseType,
   InteractionType
 } from "discord-api-types/v10";
 import { Env, InteractionResponse } from "./types";
 import { Router } from "itty-router";
+import commandHandler from "../handlers/command";
+import { respond, respondWithMessage } from "../utils/functions";
 
 const router = Router();
 
 router.get("/", () => new Response("No."));
 
-router.post("/", async function (request: Request, _env: Env): Promise<InteractionResponse> {
-  const message: APIApplicationCommandInteraction | APIPingInteraction = await request.json();
+router.post("/", async function (request: Request, env: Env): Promise<InteractionResponse> {
+  const interaction: APIInteraction = await request.json();
 
-  if (message.type === InteractionType.Ping) {
-    console.log("Handling Ping request");
-    return new InteractionResponse({ type: InteractionResponseType.Pong });
+  switch (interaction.type) {
+    case InteractionType.Ping:
+      return respond({ type: InteractionResponseType.Pong });
+
+    case InteractionType.ApplicationCommand:
+      return commandHandler(interaction, env);
+
+    // TODO: Handle other interaction types.
+    // case InteractionType.MessageComponent:
+    //   return ;
+
+    // case InteractionType.ApplicationCommandAutocomplete:
+    //   return ;
+
+    // case InteractionType.ModalSubmit:
+    //   return ;
+
+    default:
+      return respondWithMessage({ content: "Not implemented yet." });
   }
-  return new InteractionResponse({
-    type: InteractionResponseType.ChannelMessageWithSource,
-    data: { content: "Testing workflow!" }
-  });
 });
 
 export default router;
